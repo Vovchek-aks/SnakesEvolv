c1a78e80c02e00fc8270a171498b7bf2b6a695ff
Vovchek <Vlad.aks03@mail.ru>
2021-09-22 21:46:03 +0500

    cum it

--- main.py
+++ main.py
@@ -1,16 +1,170 @@
-# This is a sample Python script.
+from enum import Enum
+import pygame as pg
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from settings import *
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class CellType(Enum):
+    border = 0,
+    snake = 1,
+    eat = 2
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Cell:
+    eat_cells = []
+    border_cells = []
+
+    def __init__(self, pos: tuple, cell_type=CellType.border):
+        self.type = cell_type
+        self.pos = pos
+
+    @staticmethod
+    def find_cell_pos(cells: list, pos: tuple):
+        for c in cells:
+            if c.pos == pos:
+                return c
+
+        return None
+
+    @classmethod
+    def get_all_cells(cls) -> list:
+        all_c = cls.eat_cells + cls.border_cells
+        for s in Snake.snakes:
+            all_c += s.cells
+
+        return all_c
+
+    @classmethod
+    def add_cell(cls, cell):
+        if cell.type == CellType.border:
+            cls.border_cells += [cell]
+        elif cell.type == CellType.eat:
+            cls.eat_cells += [cell]
+
+
+class Dirs(Enum):
+    up = 0,
+    down = 1,
+    right = 2,
+    left = 3
+
+
+dirs_dirs = {
+    Dirs.up: (0, -1),
+    Dirs.down: (0, 1),
+    Dirs.right: (1, 0),
+    Dirs.left: (-1, 0)
+}
+
+
+class Snake:
+    snakes = []
+
+    def __init__(self, cells: list):
+        self.cells = cells
+        Snake.snakes += [self]
+
+    def __del__(self):
+        if self in Snake.snakes:
+            Snake.snakes.remove(self)
+
+    def move(self, pos):
+        for i in range(len(self.cells) - 1, 0, -1):
+            self.cells[i].pos = self.cells[i - 1].pos
+
+        self.cells[0].pos = pos
+
+    def die(self):
+        for c in self.cells:
+            c.type = CellType.eat
+            Cell.add_cell(c)
+
+        Snake.snakes.remove(self)
+
+    def eat(self, pos):
+        tail = self.cells[-1].pos
+        Cell.eat_cells.remove(Cell.find_cell_pos(Cell.eat_cells, pos))
+        self.move(pos)
+        self.cells += [Cell(tail, CellType.snake)]
+
+    def go(self, dir_):
+        pos = self.cells[0].pos
+        pos = pos[0] + dirs_dirs[dir_][0], pos[1] + dirs_dirs[dir_][1]
+
+        c = Cell.find_cell_pos(Cell.get_all_cells(), pos)
+
+        if c.__class__ == Cell:
+            if c.type == CellType.eat:
+                self.eat(pos)
+            elif c.type in (CellType.border, CellType.snake):
+                self.die()
+        else:
+            self.move(pos)
+
+
+class Drawer:
+    colors = {
+        CellType.border: border_color,
+        CellType.eat: eat_color,
+        CellType.snake: snake_color,
+    }
+
+    @staticmethod
+    def draw_lines():
+        for x in range(1, n_w_cells):
+                pg.draw.rect(sc, line_color, (x * cell_size, 0, 1, height))
+
+        for y in range(1, n_h_cells):
+                pg.draw.rect(sc, line_color, (0, y * cell_size, width, 1))
+
+    @staticmethod
+    def draw_cells(cells: list, color=()):
+        for c in cells:
+            pg.draw.rect(sc, color if color else Drawer.colors.get(c.type, error_color),
+                         (c.pos[0] * cell_size, c.pos[1] * cell_size, cell_size, cell_size))
+
+
+pg.init()
+sc = pg.display.set_mode(size)
+
+pg.display.set_caption('Snakes')
+
+clock = pg.time.Clock()
+
+for y in range(5):
+    y *= 5
+
+    Cell.add_cell(Cell((10, y + 10), CellType.eat))
+
+for x in range(0, n_w_cells):
+    Cell.add_cell(Cell((x, 0)))
+    Cell.add_cell(Cell((x, n_h_cells - 1)))
+
+for y in range(0, n_h_cells):
+    Cell.add_cell(Cell((0, y)))
+    Cell.add_cell(Cell((n_w_cells - 1, y)))
+
+
+Snake([Cell((10, y), CellType.snake) for y in range(40, 44)])
+
+
+while True:
+    sc.fill(bg_color)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit(0)
+        elif event.type == pg.MOUSEMOTION:
+            pass
+
+    if Snake.snakes:
+        Snake.snakes[0].go(Dirs.up if Snake.snakes[0].cells[0].pos[1] >= 2 else Dirs.right)
+
+    Drawer.draw_cells(Cell.get_all_cells())
+    Drawer.draw_lines()
+
+    pg.display.flip()
+    clock.tick(20)
+
+
+
+
---
+++ settings.py
@@ -0,0 +1,22 @@
+#
+
+
+n_w_cells = 96
+n_h_cells = 60
+cell_size = 16
+
+size = width, height = n_w_cells * cell_size, n_h_cells * cell_size
+
+white = 255, 255, 255
+red = 255, 0, 0
+
+bg_color = (102, 153, 102)
+line_color = (150, 200, 150)
+
+border_color = 70, 100, 30
+eat_color = 170, 70, 10
+snake_color = 50, 70, 200
+
+error_color = 255, 0, 200
+
+
