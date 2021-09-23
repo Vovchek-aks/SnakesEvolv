d5447198573a86b2e42b19060ef71e9b8e718362
Vovchek <Vlad.aks03@mail.ru>
2021-09-23 19:42:50 +0500

    gut

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from enum import Enum
 import pygame as pg
+from random import randint
 
 from settings import *
+from very_useful_funcs import *
 
 
 class CellType(Enum):
     border = 0,
     snake = 1,
-    eat = 2
+    eat = 2,
+    empty = 3
 
 
 class Cell:
@@ -57,11 +60,58 @@ dirs_dirs = {
 }
 
 
+class Sensor:
+    dirs = (Dirs.up, Dirs.right, Dirs.down, Dirs.left)
+
+    @classmethod
+    def generate(cls, sz: int, s_type=CellType.empty) -> tuple:
+        s = []
+        for x in range(-sz, sz):
+            for y in range(-sz, sz):
+                if not (x == y == 0):
+                    s += [Sensor((x, y), tuple([randint(-64, 64) for i in range(4)]), s_type)]
+        return tuple(s)
+
+    @classmethod
+    def generate_all(cls, sz: int) -> tuple:
+        s = ()
+        for i in CellType:
+            s = s + cls.generate(sz, i)
+        return s
+
+    def __init__(self, pos: tuple, values: tuple, cell_type=CellType.empty):
+        self.pos = pos
+        self.type = cell_type
+        self.values = values
+
+    def view(self, d_pos: tuple) -> tuple:
+        c = Cell.find_cell_pos(Cell.get_all_cells(), sum_tuple(d_pos, self.pos))
+        return self.values if c and c.type == self.type else (0, 0, 0, 0)
+
+
 class Snake:
     snakes = []
 
-    def __init__(self, cells: list):
+    @classmethod
+    def generate_snake(cls, pos: tuple, dir_=Dirs.down, l_=4):
+        c = []
+        for i in range(l_):
+            c += [Cell(pos, CellType.snake)]
+            pos = sum_tuple(pos, dirs_dirs[dir_])
+
+        return Snake(c, Sensor.generate_all(1))
+
+    @classmethod
+    def update_all(cls):
+        i = 0
+        while i < len(cls.snakes):
+            if cls.snakes[i].update():
+                i += 1
+
+    def __init__(self, cells: list, sensors: tuple):
         self.cells = cells
+        self.sensors = sensors
+
         Snake.snakes += [self]
 
     def __del__(self):
@@ -87,9 +137,20 @@ class Snake:
         self.move(pos)
         self.cells += [Cell(tail, CellType.snake)]
 
-    def go(self, dir_):
+        if len(self.cells) >= 10:
+            self.replication()
+
+    def replication(self):
+        s = self.cells[len(self.cells) // 2:][::-1]
+        self.cells = self.cells[:len(self.cells) // 2]
+
+        Snake(s, self.sensors)
+
+    def go(self, dir_) -> bool:
+        f = True
+
         pos = self.cells[0].pos
-        pos = pos[0] + dirs_dirs[dir_][0], pos[1] + dirs_dirs[dir_][1]
+        pos = sum_tuple(pos, dirs_dirs[dir_])
 
         c = Cell.find_cell_pos(Cell.get_all_cells(), pos)
 
@@ -98,8 +159,30 @@ class Snake:
                 self.eat(pos)
             elif c.type in (CellType.border, CellType.snake):
                 self.die()
+                f = False
         else:
             self.move(pos)
+
+        return f
+
+    def update(self) -> bool:
+        sum_ = (0, 0, 0, 0)
+        for i in self.sensors:
+            sum_ = sum_tuple(sum_, i.view(self.cells[0].pos))
+
+        return self.go(Sensor.dirs[sorted(enumerate(sum_), key=lambda x: -x[1])[0][0]])
+
+
+class Field:
+    @staticmethod
+    def gen_borders():
+        for x in range(0, n_w_cells):
+            Cell.add_cell(Cell((x, 0)))
+            Cell.add_cell(Cell((x, n_h_cells - 1)))
+
+        for y in range(0, n_h_cells):
+            Cell.add_cell(Cell((0, y)))
+            Cell.add_cell(Cell((n_w_cells - 1, y)))
 
 
 class Drawer:
@@ -131,21 +214,15 @@ pg.display.set_caption('Snakes')
 
 clock = pg.time.Clock()
 
-for y in range(5):
-    y *= 5
-
-    Cell.add_cell(Cell((10, y + 10), CellType.eat))
-
-for x in range(0, n_w_cells):
-    Cell.add_cell(Cell((x, 0)))
-    Cell.add_cell(Cell((x, n_h_cells - 1)))
-
-for y in range(0, n_h_cells):
-    Cell.add_cell(Cell((0, y)))
-    Cell.add_cell(Cell((n_w_cells - 1, y)))
+Field.gen_borders()
 
 
-Snake([Cell((10, y), CellType.snake) for y in range(40, 44)])
+for x in range(45):
+    Snake.generate_snake((4 + x * 2, 30), Dirs.down)
+    Snake.generate_snake((4 + x * 2, 31))
+
+# for y in range(20):
+#     Cell.add_cell(Cell((10, 10 + y), CellType.eat))
 
 
 while True:
@@ -156,14 +233,13 @@ while True:
         elif event.type == pg.MOUSEMOTION:
             pass
 
-    if Snake.snakes:
-        Snake.snakes[0].go(Dirs.up if Snake.snakes[0].cells[0].pos[1] >= 2 else Dirs.right)
+    Snake.update_all()
 
     Drawer.draw_cells(Cell.get_all_cells())
     Drawer.draw_lines()
 
     pg.display.flip()
-    clock.tick(20)
+    clock.tick(10)
 
 
 
---
+++ very_useful_funcs.py
@@ -0,0 +1,3 @@
+def sum_tuple(t1: tuple, t2: tuple):
+    return tuple([t1[i] + t2[i] for i in range(len(t1))])
+
